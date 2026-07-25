a15c09754d0c3dc3631c36ede84aa71a04738be3
aya el azzouzi <ayaazzouzi39@gmail.com>
2026-07-25 19:20:01 +0100

    implémentation du side and navbar avec light et dark mode

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,12 @@ import { Missions } from './features/missions/missions';
 import { Etablissements } from './features/etablissements/etablissements';
 import { ChatIa } from './features/chat-ia/chat-ia';
 
+// 1. Importe tes composants manquants
+import { Stock } from './features/stock/stock';
+import { Users } from './features/users/users';
+import { Guides } from './features/guides/guides';
+import { Settings } from './features/settings/settings';
+
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: Auth },
@@ -12,4 +18,13 @@ export const routes: Routes = [
   { path: 'missions', component: Missions },
   { path: 'etablissements', component: Etablissements },
   { path: 'chat-ia', component: ChatIa },
+
+  // 2. Ajoute les routes correspondantes
+  { path: 'stock', component: Stock },
+  { path: 'users', component: Users },
+  { path: 'guides', component: Guides },
+  { path: 'settings', component: Settings },
+
+  // Facultatif : Redirection si l'URL saisie n'existe pas
+  { path: '**', redirectTo: 'dashboard' }
 ];
