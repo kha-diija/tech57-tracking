0d5cc65bccf334fe07204e1d9b68376d257002c9
assia769 <ahoubbadi139@gmail.com>
2026-08-02 12:52:08 +0100

    correction de simulateur

--- frontend/src/app/shared/components/sidebar/sidebar.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter, inject, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service'; // ⚠️ adapte selon le chemin réel depuis sidebar/
+import { AuthService } from '../../services/auth.service'; 
 import {
   LucideAngularModule,
   LayoutDashboard,
@@ -15,9 +15,12 @@ import {
   LogOut,
   PanelLeft,
   Sparkles,
-   Route
+  Route,
+  ClipboardList,
+  PackageSearch
+
 } from 'lucide-angular';
-import { UserRole } from '../../models/auth.model'; // ⚠️ adapte selon le chemin réel
+import { UserRole } from '../../models/auth.model'; 
 
 interface NavItem {
   label: string;
@@ -44,19 +47,19 @@ export class Sidebar {
     PanelLeft,
     Sparkles
   };
-
- private readonly allNavItems: NavItem[] = [
-  { label: 'Tableau de bord', route: '/dashboard', icon: LayoutDashboard, roles: ['ADMINISTRATEUR', 'TECHNICIEN', 'OBSERVATEUR', 'GESTIONNAIRE_STOCK'] },
-  { label: 'Établissements', route: '/etablissements', icon: Building2, roles: ['ADMINISTRATEUR'] },
-  { label: 'Missions', route: '/missions', icon: Briefcase, roles: ['ADMINISTRATEUR', 'TECHNICIEN'] },
-  { label: 'Simulateur de trajet', route: '/simulateur-trajet', icon: Route, roles: ['ADMINISTRATEUR', 'TECHNICIEN'] }, // 👈 ajout
+private readonly allNavItems: NavItem[] = [
+  { label: 'Tableau de bord', route: '/admin/dashboard', icon: LayoutDashboard, roles: ['ADMINISTRATEUR', 'TECHNICIEN', 'OBSERVATEUR', 'GESTIONNAIRE_STOCK'] },
+  { label: 'Établissements', route: '/admin/etablissements', icon: Building2, roles: ['ADMINISTRATEUR'] },
+  { label: 'Missions', route: '/admin/missions', icon: Briefcase, roles: ['ADMINISTRATEUR', 'TECHNICIEN'] },
+  { label: 'Simulateur de trajet', route: '/admin/simulateur-trajet', icon: Route, roles: ['ADMINISTRATEUR', 'TECHNICIEN'] },
   { label: 'Stock & Matériel', route: '/stock', icon: Package, roles: ['ADMINISTRATEUR', 'GESTIONNAIRE_STOCK'] },
   { label: 'Utilisateurs', route: '/users', icon: Users, roles: ['ADMINISTRATEUR'] },
   { label: 'Assistant IA', route: '/chat-ia', icon: Bot, roles: ['ADMINISTRATEUR', 'TECHNICIEN', 'OBSERVATEUR', 'GESTIONNAIRE_STOCK'] },
+  { label: 'Gestion des ressources', route: '/admin/ressources', icon: PackageSearch, roles: ['ADMINISTRATEUR', 'GESTIONNAIRE_STOCK'] },
+  { label: 'Gestion et suivi des interventions', route: '/admin/interventions', icon: ClipboardList, roles: ['ADMINISTRATEUR', 'TECHNICIEN'] },
   { label: 'Guides & Support', route: '/guides', icon: BookOpen, roles: ['ADMINISTRATEUR', 'TECHNICIEN', 'OBSERVATEUR', 'GESTIONNAIRE_STOCK'] },
   { label: 'Paramètres', route: '/settings', icon: Settings, roles: ['ADMINISTRATEUR', 'TECHNICIEN', 'OBSERVATEUR', 'GESTIONNAIRE_STOCK'] }
 ];
-
   navItems = computed(() => {
     const role = this.authService.currentUser()?.role;
     if (!role) return [];
