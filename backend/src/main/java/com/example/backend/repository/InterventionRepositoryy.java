6bf45d1c6c57003f04dee0b51a076dfaa6bafa92
assia769 <ahoubbadi139@gmail.com>
2026-09-02 18:12:51 +0100

    optimisation des requets partenaires+fix de bug gestionnaire de stock

--- backend/src/main/java/com/example/backend/repository/InterventionRepositoryy.java
+++ backend/src/main/java/com/example/backend/repository/InterventionRepositoryy.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface InterventionRepositoryy extends JpaRepository<Intervention, Integer> {
 
     @Query("select avg(i.tauxAvancement) from Intervention i " +
@@ -13,5 +15,11 @@ public interface InterventionRepositoryy extends JpaRepository<Intervention, Int
 
     @Query("select avg(i.tauxAvancement) from Intervention i " +
             "where i.mission.etablissement.commune.province.idProvince = :idProvince")
+
     Double findAvgTauxAvancementByProvince(@Param("idProvince") Integer idProvince);
+
+    @Query("select i.mission.etablissement.idEtablissement, avg(i.tauxAvancement) from Intervention i " +
+            "where i.mission.etablissement.idEtablissement in :ids " +
+            "group by i.mission.etablissement.idEtablissement")
+    List<Object[]> findAvgTauxAvancementByEtablissementIds(@Param("ids") List<Integer> ids);
 }
